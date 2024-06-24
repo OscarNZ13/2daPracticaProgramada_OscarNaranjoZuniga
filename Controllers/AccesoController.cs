32b2d8f6b9f75dbaac447cbdb583b9e6316f492d
OscarNZ13 <onaranjo00330@ufide.ac.cr>
2024-06-24 14:06:17 -0600

    Login funcional

--- Controllers/AccesoController.cs
+++ Controllers/AccesoController.cs
@@ -1,14 +1,16 @@
 ﻿using _2daPracticaProgramada_OscarNaranjoZuniga.Models;
+using _2daPracticaProgramada_OscarNaranjoZuniga.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace _2daPracticaProgramada_OscarNaranjoZuniga.Controllers
 {
     public class AccesoController : Controller
     {
+        //Con esta variable del nuestro contexto es la que se va a utilizar para realizar acciones con la DB
         private readonly ApplicationDbContext _appDbContext;
 
-        public AccesoController(ApplicationDbContext AppDbContext) 
+        public AccesoController(ApplicationDbContext AppDbContext)
         {
             _appDbContext = AppDbContext;
         }
@@ -23,10 +25,12 @@ namespace _2daPracticaProgramada_OscarNaranjoZuniga.Controllers
         public async Task<IActionResult> Registro(Usuario Usuario)
         {
             //Se crea el objeto con la informacion del post
-            Usuario User = new Usuario() 
-            {Nombre = Usuario.Nombre,
-            Email = Usuario.Email,
-            Contrasena = Usuario.Contrasena};
+            Usuario User = new Usuario()
+            {
+                Nombre = Usuario.Nombre,
+                Email = Usuario.Email,
+                Contrasena = Usuario.Contrasena
+            };
 
             //Se envia a la db
             await _appDbContext.Usuarios.AddAsync(Usuario);
@@ -37,11 +41,34 @@ namespace _2daPracticaProgramada_OscarNaranjoZuniga.Controllers
             {
                 return RedirectToAction("Login", "Acceso");
             }
-            else 
+            else
             {
-                ViewData["Mensaje"]= "No sé pudo crear el usuario";
+                ViewData["Mensaje"] = "No sé pudo crear el usuario";
                 return View();
             }
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM Usuario)
+        {
+            Usuario? Usuario_Recibido = await _appDbContext.Usuarios.Where(u => u.Email == Usuario.Email 
+                                                                            && u.Contrasena == Usuario.Contrasena)      
+                                                                            .FirstOrDefaultAsync(); // Si lo encuentra me lo devuelve, sino devuelve un null
+
+            //Aqui validamos que nos ha devuelto la consulta
+            if(Usuario_Recibido == null)
+            {
+                ViewData["Mensaje"] = "Error al encontrar el usuario";
+                return View();
+            }
+            
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ Models/ViewModels/LoginVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _2daPracticaProgramada_OscarNaranjoZuniga.Models.ViewModels
+{
+    public class LoginVM
+    {
+        public required string Email { get; set; }
+
+        public required string Contrasena { get; set; }
+
+    }
+}
